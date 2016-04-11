e4693e510445f5df9ff8ea3971b1082dfde1c48e
Florent NOLOT <fnolot@univ-reims.fr>
2016-04-11 19:50:14 +0200

    Avancement pour Modification mot de passe

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -121,6 +121,58 @@ class DefaultController extends Controller
 		
 	}
 	
+	/**
+     * @Route("/admin/change_passwd", name="change_passwd")
+     */
+	function changepasswd() {
+		$user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->get('event_dispatcher');
+		
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_INITIALIZE, $event);
+		
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+		
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->get('fos_user.profile.form.factory');
+		
+        $form = $formFactory->createForm();
+        $form->setData($user);
+		
+        $form->handleRequest($request);
+		
+        if ($form->isValid()) {
+            /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+            $userManager = $this->get('fos_user.user_manager');
+			
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_SUCCESS, $event);
+			
+            $userManager->updateUser($user);
+			
+            if (null === $response = $event->getResponse()) {
+                $url = $this->generateUrl('fos_user_profile_show');
+                $response = new RedirectResponse($url);
+            }
+			
+            $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+			
+            return $response;
+        }
+		
+        return $this->render('UserBundle:Profile:edit.html.twig', array(
+		'form' => $form->createView(),
+		'user' => $user
+        ));
+	}
+	
 	function stripAccents($str, $encoding='utf-8')
 	{
 	    // transformer les caractères accentués en entités HTML
