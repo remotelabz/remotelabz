8c1a3605ed41fd3420d7930e878c40c226b15d21
root <root@univ-reims.fr>
2016-04-20 16:41:40 +0200

    test 20A

---
+++ src/TestBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('TestBundle:Default:index.html.twig');
+    }
+}
