d0009787fa5ae655fe6ba9c2fab1651a6ee2a792
Florent Nolot <www-data@univ-reims.fr>
2016-05-16 14:14:27 +0200

    Création de nouveaux bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,8 @@ class AppKernel extends Kernel
 			new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new TestBundle\TestBundle(),
             new BackendBundle\BackendBundle(),
+            new ControlBundle\ControlBundle(),
+            new VerifyBundle\VerifyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
