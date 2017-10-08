662ac39649f872c3ea038055f70441f1bd5da965
www-data <www-data@univ-reims.fr>
2017-10-08 22:22:26 +0200

    Préparation de la gestion de TP avec IP configurée et VPN pour User

---
+++ src/AppBundle/Entity/NetworkUsed.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+/**
+ * NetworkUsed
+ *
+ * @ORM\Table(name="network_used")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\NetworkUsedRepository")
+ */
+class NetworkUsed
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ip_address", type="string", length=255, unique=true)
+	 * @Assert\Ip
+     */
+    private $ipAddress;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="netmask", type="string", length=255)
+     */
+    private $netmask;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set ipAddress
+     *
+     * @param string $ipAddress
+     *
+     * @return NetworkUsed
+     */
+    public function setIpAddress($ipAddress)
+    {
+        $this->ipAddress = $ipAddress;
+
+        return $this;
+    }
+
+    /**
+     * Get ipAddress
+     *
+     * @return string
+     */
+    public function getIpAddress()
+    {
+        return $this->ipAddress;
+    }
+
+    /**
+     * Set netmask
+     *
+     * @param string $netmask
+     *
+     * @return NetworkUsed
+     */
+    public function setNetmask($netmask)
+    {
+        $this->netmask = $netmask;
+
+        return $this;
+    }
+
+    /**
+     * Get netmask
+     *
+     * @return string
+     */
+    public function getNetmask()
+    {
+        return $this->netmask;
+    }
+}
