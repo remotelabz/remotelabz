30ac50eda80484d064e0c0f7542fc729d03bbfb4
MarcadiaInc <julien.hubert@outlook.com>
2019-01-23 16:53:36 +0100

    Switched to chmod

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 
 cd /app
-gosu composer install
-gosu yarn install
-gosu yarn encore dev
-# chmod 777 -R vendor/
-# chmod 777 -R node_modules/
+composer install
+yarn install
+yarn encore dev
+chmod 777 -R vendor/
+chmod 777 -R node_modules/
+chmod 777 -R public/build/
 /app/bin/console make:migration
 /app/bin/console doctrine:migrations:migrate -n
 /app/bin/console doctrine:fixtures:load -n
