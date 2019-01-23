d7c7c894e494f773ac7defcd90186de4546e2a15
MarcadiaInc <julien.hubert@outlook.com>
2019-01-23 16:57:43 +0100

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ cd /app
 composer install
 yarn install
 yarn encore dev
-chmod 777 -R vendor/
-chmod 777 -R node_modules/
-chmod 777 -R public/build/
+chmod 777 -R /app/vendor/
+chmod 777 -R /app/node_modules/
+chmod 777 -R /app/public/build/
 /app/bin/console make:migration
 /app/bin/console doctrine:migrations:migrate -n
 /app/bin/console doctrine:fixtures:load -n
