ad9e01b5c0cea6bcd0d7e4b3c5508952ab2a1d39
MarcadiaInc <julien.hubert@outlook.com>
2020-04-27 10:57:47 +0200

    Fixed lab creation error in prod env

--- src/DataFixtures/LabFixtures.php
+++ src/DataFixtures/LabFixtures.php
@@ -8,16 +8,29 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
-class LabFixtures extends Fixture implements DependentFixtureInterface, FixtureGroupInterface
+class LabFixtures extends Fixture implements DependentFixtureInterface, FixtureGroupInterface, ContainerAwareInterface
 {
+    /**
+     * The dependency injection container.
+     *
+     * @var ContainerInterface
+     */
+    protected $container;
+
     public const COUNT = 5;
 
     public function load(ObjectManager $manager)
     {
-        $faker = Factory::create();
+        /** @var KernelInterface $kernel */
+        $kernel = $this->container->get('kernel');
 
-        $description = <<<EOF
+        if (in_array($kernel->getEnvironment(), ['dev', 'test'])) {
+            $faker = Factory::create();
+
+            $description = <<<EOF
 # Scelerisque porttitor tincidunt potenti a sa
 
 Lorem ipsum dolor sit amet consectetur adipiscing elit mattis mus natoque, hendrerit purus elementum odio aliquam eros proin nulla ridiculus erat, facilisis litora tincidunt praesent sollicitudin habitasse luctus cum hac. Erat sociis sagittis cras cursus duis a, morbi quis etiam aliquam facilisi, tellus enim varius platea montes. Curae placerat felis convallis velit netus fermentum et praesent eros, in nostra porta dictumst proin lobortis dictum bibendum, ligula fusce massa scelerisque ornare cum odio euismod. 
@@ -39,19 +52,19 @@ Taciti ad quisque litora duis viverra erat volutpat arcu vestibulum, leo himenae
 Odio penatibus commodo bibendum nullam pellentesque arcu, consequat per nibh fames turpis dignissim lectus, primis sagittis fermentum at laoreet. Interdum magnis sociosqu ut quis semper platea commodo taciti vulputate, leo neque proin nulla ad vestibulum posuere faucibus lectus, inceptos cras per integer pulvinar nunc accumsan ligula. Inceptos justo velit metus mattis eleifend himenaeos neque quam a leo, est et vivamus sapien curabitur duis iaculis tellus.
 EOF;
 
-        foreach (range(1, self::COUNT) as $number) {
-            $lab = Lab::create()
-                ->setName($faker->words(2, true))
-                ->setDescription($description)
-                ->setAuthor($this->getReference('user' . $faker->numberBetween(0,9)))
-            ;
+            foreach (range(1, self::COUNT) as $number) {
+                $lab = Lab::create()
+                    ->setName($faker->words(2, true))
+                    ->setDescription($description)
+                    ->setAuthor($this->getReference('user' . $faker->numberBetween(0, 9)));
 
-            $manager->persist($lab);
+                $manager->persist($lab);
 
-            $this->addReference('lab' . $number, $lab);
+                $this->addReference('lab' . $number, $lab);
+            }
+
+            $manager->flush();
         }
-
-        $manager->flush();
     }
 
     public function getDependencies()
@@ -64,5 +77,13 @@ EOF;
     public static function getGroups(): array
     {
         return ['labs'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
 }
