a1a69e7ea75054d1cf7593ff235dfbfedd8b4220
Florent NOLOT <florent.nolot@univ-reims.fr>
2020-07-26 17:38:15 +0000

    Add Ctrl Alt Sup button

--- assets/js/vnc.js
+++ assets/js/vnc.js
@@ -10,4 +10,16 @@ var rfb = new RFB($('#noVNC_screen')[0], host + ':' + port + '/' + path, {
     clipViewport: true,
 });
 rfb.scaleViewport = true;
+console.log("in vnc.js");
+
+    var el = document.getElementById('noVNC_button')
+            clickerFn = function() {
+                if (el.value=="CtrlAltDel") {
+                    console.log("click "+el.value);
+                    rfb.sendCtrlAltDel();
+                }
+                else console.log("Alert");
+            }
+
+        el.addEventListener('click', clickerFn);
 
