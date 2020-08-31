f861e84bec8202815b273002a07b8f5af9bd637c
Julien Hubert <3481141+Atlanta@users.noreply.github.com>
2020-08-31 11:54:54 +0200

    Revert "delete network interfaces before device"

--- src/Controller/DeviceController.php
+++ src/Controller/DeviceController.php
@@ -222,12 +222,6 @@ class DeviceController extends Controller
         }
 
         $entityManager = $this->getDoctrine()->getManager();
-
-        foreach ($device->getNetworkInterfaces() as $networkInterface) {
-            $entityManager->remove($networkInterface);
-        }
-
-        $entityManager->flush();
         $entityManager->remove($device);
         $entityManager->flush();
 
