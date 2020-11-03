d816978581eb2c153fdb5c8d64d7afb8b7330f95
Julien Hubert <3481141+Atlanta@users.noreply.github.com>
2020-11-03 16:05:08 +0100

    Fixed #567

--- assets/js/vnc.js
+++ assets/js/vnc.js
@@ -1,25 +1,44 @@
 import RFB from '@novnc/novnc/core/rfb'
 
+var rfb;
+
+function connectToVNC(host, port, path) {
+    console.log('Connecting to ' + host + ':' + port + '/' + path + '...');
+    rfb = new RFB($('#noVNC_screen')[0], host + ':' + port + '/' + path, {
+        scaleViewport: true,
+        clipViewport: true,
+    });
+    rfb.scaleViewport = true;
+    rfb.addEventListener('connect', function () {
+        reconnectButton.setAttribute("disabled", "disabled");
+        console.log("Event: RFB connected");
+    });
+    rfb.addEventListener('disconnect', function () {
+        console.log("Event: RFB disconnect");
+        rfb = null;
+        reconnectButton.removeAttribute("disabled");
+    });
+}
+
+var el = document.getElementById('CtrlAltDelButton');
+el.onclick = () => {
+    rfb.sendCtrlAltDel();
+    console.log("User action: sent Ctrl+Alt+Del");
+}
+
+var reconnectButton = document.getElementById('ReconnectButton');
+reconnectButton.onclick = () => {
+    console.log("User action: reconnect to VNC");
+    reconnectButton.setAttribute("disabled", "disabled");
+    var userRating = document.querySelector('.js-user-rating');
+    var host = userRating.dataset.host;
+    var port = userRating.dataset.port;
+    var path = userRating.dataset.path;
+    connectToVNC(host, port, path);
+}
+
 var userRating = document.querySelector('.js-user-rating');
 var host = userRating.dataset.host;
 var port = userRating.dataset.port;
 var path = userRating.dataset.path;
-console.log('Connecting to ' + host + ':' + port + '/' + path + '...');
-var rfb = new RFB($('#noVNC_screen')[0], host + ':' + port + '/' + path, {
-    scaleViewport: true,
-    clipViewport: true,
-});
-rfb.scaleViewport = true;
-console.log("in vnc.js");
-
-    var el = document.getElementById('noVNC_button')
-            clickerFn = function() {
-                if (el.value=="CtrlAltDel") {
-                    console.log("click "+el.value);
-                    rfb.sendCtrlAltDel();
-                }
-                else console.log("Alert");
-            }
-
-        el.addEventListener('click', clickerFn);
-
+connectToVNC(host, port, path);
