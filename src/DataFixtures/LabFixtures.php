60c9646063a849afb1182b0fe8af41de909b9cb7
Florent <florent.nolot@univ-reims.fr>
2022-01-27 17:49:11 +0100

    fixture set solved

--- src/DataFixtures/LabFixtures.php
+++ src/DataFixtures/LabFixtures.php
@@ -27,7 +27,7 @@ class LabFixtures extends Fixture implements DependentFixtureInterface, FixtureG
         /** @var KernelInterface $kernel */
         $kernel = $this->container->get('kernel');
 
-        if (in_array($kernel->getEnvironment(), ['dev', 'test'])) {
+   /*     if (in_array($kernel->getEnvironment(), ['dev', 'test'])) {
             $faker = Factory::create();
 
             $description = <<<EOF
@@ -67,6 +67,7 @@ EOF;
 
             $manager->flush();
         }
+        */
     }
 
     public function getDependencies()
