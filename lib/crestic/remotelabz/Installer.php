bc6d37c2bf6191187045d78ee2ae599f1acc7cb5
Florent <florent.nolot@univ-reims.fr>
2022-03-13 14:15:30 +0100

    Install corrected

--- lib/crestic/remotelabz/Installer.php
+++ lib/crestic/remotelabz/Installer.php
@@ -357,7 +357,7 @@ class Installer
         }
 
         // Handle PHP max upload filesize
-        $phpPath = str_replace(["cli", ",", "\n"], ["mods-available", "", ""], shell_exec("php --ini | grep fileinfo"));
+        $phpPath = str_replace(["cli/conf.d/20-", ",", "\n"], ["mods-available/", "", ""], shell_exec("php --ini | grep fileinfo"));
         $postMaxSize = intval(intval(substr($uploadMaxFilesize, 0, -1)) * 1.25);
         $ini = parse_ini_file($phpPath);
         // If keys already exists
@@ -400,7 +400,7 @@ class Installer
             throw new Exception("Could not restart install Yarn packages correctly.");
         }
         unset($output);
-        exec("yarn encore dev 2>&1", $output, $returnCode);
+        exec("yarn encore dev", $output, $returnCode);
         $this->logger->debug($output);
         if ($returnCode) {
             throw new Exception("Could not compile Yarn packages correctly.");
