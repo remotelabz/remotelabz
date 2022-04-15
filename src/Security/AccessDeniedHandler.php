d3f25581fec5dd7d2e2473cb3810e1f807d1e18b
Florent <florent.nolot@univ-reims.fr>
2022-04-15 19:23:48 +0200

    Teacher have to access to sandbox Fixes #764

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -7,19 +7,24 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
-//use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Security\Core\Security;
+use Psr\Log\LoggerInterface;
 
 class AccessDeniedHandler implements AccessDeniedHandlerInterface
 {
+    private $logger;
+
+    public function __construct(
+        LoggerInterface $logger
+    ) {
+        $this->logger = $logger;
+    }
+
     public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
     {
         
-        $this->addFlash("warning","Access denied");
-        if ($request->hasSession()) {
-            $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
-        }
-        $this->logger->debug("Access denied");
-
+        $this->logger->info("Exception AccessDeniedHandler, request :".$request->query->get('GET'));
+        
         return new RedirectResponse('/');
     }
 }
