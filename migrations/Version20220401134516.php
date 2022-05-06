90797f0a6e065049f6f436b94bc241d2b9a0b201
Florent <florent.nolot@univ-reims.fr>
2022-05-06 16:27:37 +0200

    Fixed migration file

--- migrations/Version20220401134516.php
+++ migrations/Version20220401134516.php
@@ -25,8 +25,8 @@ final class Version20220401134516 extends AbstractMigration
         $this->addSql('ALTER TABLE device ADD hypervisor_id INT DEFAULT NULL, DROP hypervisor');
         $this->addSql('ALTER TABLE device ADD CONSTRAINT FK_92FB68E4F2DF8A7 FOREIGN KEY (hypervisor_id) REFERENCES hypervisor (id)');
         $this->addSql('CREATE INDEX IDX_92FB68E4F2DF8A7 ON device (hypervisor_id)');
-        $this->addSql('UPDATE device SET hypervisor_id = 3 WHERE type=\'vm\'');
-        $this->addSql('UPDATE device SET hypervisor_id = 4 WHERE type=\'container\'');
+        $this->addSql('UPDATE device SET hypervisor_id = 1 WHERE type=\'vm\'');
+        $this->addSql('UPDATE device SET hypervisor_id = 2 WHERE type=\'container\'');
 
     }
 
