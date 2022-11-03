d140d3ed9ca49a966f81510fc63f0c78ccc90ad8
Florent <florent.nolot@univ-reims.fr>
2022-11-03 21:50:24 +0100

    Delete old organization name in lib

--- lib/autoload.php
+++ lib/autoload.php
@@ -6,4 +6,4 @@ use Paulyg\Autoloader;
 
 Autoloader::addPsr4('GetOpt',       dirname(__DIR__).'/lib/ulrichsg/getopt-php/src/');
 Autoloader::addPsr4('M1\Env',       dirname(__DIR__).'/lib/m1/Env/src/');
-Autoloader::addPsr4('RemoteLabz',   dirname(__DIR__).'/lib/crestic/remotelabz/');
+Autoloader::addPsr4('RemoteLabz',   dirname(__DIR__).'/lib/remotelabz/');
