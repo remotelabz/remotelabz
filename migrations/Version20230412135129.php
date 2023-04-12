661f3aaaeeb49b66b34b7e59a8b5c9a42fdbc4bb
Noemie-P <piccoli.noemie@gmail.com>
2023-04-12 13:57:49 +0000

    Migrations

--- migrations/Version20230412135129.php
+++ migrations/Version20230412135129.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230412132032 extends AbstractMigration
+final class Version20230412135129 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -26,7 +26,7 @@ final class Version20230412132032 extends AbstractMigration
         $this->addSql('CREATE TABLE text_object (id INT AUTO_INCREMENT NOT NULL, lab_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) DEFAULT NULL, data VARCHAR(1500) DEFAULT NULL, newdata VARCHAR(255) DEFAULT NULL, INDEX IDX_BD21321F628913D5 (lab_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE network_device ADD CONSTRAINT FK_6B3CB1E8628913D5 FOREIGN KEY (lab_id) REFERENCES lab (id) ON DELETE SET NULL');
         $this->addSql('ALTER TABLE text_object ADD CONSTRAINT FK_BD21321F628913D5 FOREIGN KEY (lab_id) REFERENCES lab (id) ON DELETE SET NULL');
-        $this->addSql('ALTER TABLE device ADD template VARCHAR(255) DEFAULT NULL, ADD config INT DEFAULT 0 NOT NULL, ADD status INT DEFAULT 0 NOT NULL, ADD ethernet INT DEFAULT 1 NOT NULL, CHANGE config_data config_data VARCHAR(255) DEFAULT \'\' NOT NULL');
+        $this->addSql('ALTER TABLE device ADD delay INT NOT NULL, ADD console VARCHAR(50) DEFAULT NULL, ADD icon VARCHAR(100) DEFAULT NULL, ADD url VARCHAR(70) DEFAULT NULL, ADD template VARCHAR(255) DEFAULT NULL, ADD image VARCHAR(255) DEFAULT NULL, ADD count INT DEFAULT NULL, ADD postfix INT DEFAULT NULL, ADD port INT DEFAULT NULL, ADD config INT DEFAULT 0 NOT NULL, ADD config_data VARCHAR(255) DEFAULT \'\' NOT NULL, ADD status INT DEFAULT 0 NOT NULL, ADD ethernet INT DEFAULT 1 NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -36,6 +36,6 @@ final class Version20230412132032 extends AbstractMigration
 
         $this->addSql('DROP TABLE network_device');
         $this->addSql('DROP TABLE text_object');
-        $this->addSql('ALTER TABLE device DROP template, DROP config, DROP status, DROP ethernet, CHANGE config_data config_data VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE device DROP delay, DROP console, DROP icon, DROP url, DROP template, DROP image, DROP count, DROP postfix, DROP port, DROP config, DROP config_data, DROP status, DROP ethernet');
     }
 }
