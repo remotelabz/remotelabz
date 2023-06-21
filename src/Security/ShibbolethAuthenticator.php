6ed27ccf78a16572c19ff2b04f7b7241be5eead8
Noemie-P <piccoli.noemie@gmail.com>
2023-06-21 07:38:50 +0000

    import userBadge in shibbolethAuthenticator

--- src/Security/ShibbolethAuthenticator.php
+++ src/Security/ShibbolethAuthenticator.php
@@ -25,6 +25,7 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 
 
 use Psr\Log\LoggerInterface;
