61b68fce4a03c7f40a8e1a3784a4d0fd075d8985
Noemie-P <piccoli.noemie@gmail.com>
2023-09-27 07:32:18 +0000

    send mail to several people and check emails

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -9,14 +9,26 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints\Email as EmailConstraint;
+use Psr\Log\LoggerInterface;
 
 class MailController extends Controller
 {
     private $mailer;
+    private $validator;
+
+    /** @var LoggerInterface $logger */
+    private $logger;
+
     public function __construct(
-        MailerInterface $mailer)
+        MailerInterface $mailer,
+        ValidatorInterface $validator,
+        LoggerInterface $logger)
     {
         $this->mailer = $mailer;
+        $this->validator = $validator;
+        $this->logger = $logger;
     }
 
     /**
@@ -34,23 +46,81 @@ class MailController extends Controller
 
         if ($mailForm->isSubmitted() && $mailForm->isValid()) {
             $mail = $mailForm->getData();
-            $this->sendMail($mail);
+            $this->checkAction($mail);
         }
 
         return $this->render('mail/index.html.twig', ['form'=>$mailForm->createView()]);
     }
 
+
+    public function checkAction($mail) {
+
+        $toAdresses = [];
+        $badAdresses = [];
+        $emailConstraint = new EmailConstraint();
+        foreach($mail['to'] as $toAdresse) {
+
+            $errors = $this->validator->validate($toAdresse, $emailConstraint);
+            if (count($errors) == 0) {
+
+                array_push($toAdresses, $toAdresse);
+            }
+            else {
+
+                array_push($badAdresses, $toAdresse);
+                $errorsString = (string) $errors;
+                var_dump($errorsString);
+            }
+        }
+
+        if (count($mail['to']) != count($toAdresses)) {
+            $this->logger->debug("Some addresses are not valid: ".print_r($badAdresses, true));
+        }
+
+        /*$ccAdresses = [];
+        foreach($mail['cc'] as $ccAdresse) {
+
+            $errors = $this->validator->validate($ccAdresse, $emailConstraint);
+            if (count($errors) == 0) {
+
+                array_push($ccAdresses, $ccAdresse);
+            }
+        }
+
+        $bccAdresses = [];
+        foreach($mail['cci'] as $bccAdresse) {
+
+            $errors = $this->validator->validate($bccAdresse, $emailConstraint);
+            if (count($errors) == 0) {
+
+                array_push($bccAdresses, $bccAdresse);
+            }
+        }*/
+
+        $checkedEmail = [
+            'to' => $toAdresses,
+            /*'cc' => $ccAdresses,
+            'cci' => $bccAdresses,*/
+            'subject' => $mail['subject'],
+            'content' => $mail['content']
+        ];
+
+        $this->sendMail($checkedEmail);
+        
+    }
+
     public function sendMail($mail) {
 
+        //var_dump($mail['to']);exit;
         $emailToSend = (new Email())
             ->from('test@exemple.org')
-            ->to($mail['to']);
-        if ($mail['cc']) {
+            ->to(...$mail['to']);
+        /*if ($mail['cc']) {
             $emailToSend->cc($mail['cc']);
         }
         if ($mail['cci']) {
             $emailToSend->bcc($mail['cci']);
-        }
+        }*/
         $emailToSend->subject($mail['subject'])
             ->html('<p>'.$mail['content'].'</p>');
 
--- src/Form/MailType.php
+++ src/Form/MailType.php
@@ -10,28 +10,63 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Form\CallbackTransformer;
 
 class MailType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('to', EmailType::class, [
-                //'attr' => ['multiple' => 'multiple'],
-                //'constraints' => [new Email(['mode'=>'strict']) ]
+            ->add('to', TextType::class, [
             ])
-            ->add('cc', EmailType::class, [
-                //'attr' => ['multiple' => 'multiple']
+            /*->add('cc', TextType::class, [
+                'required' => false
             ])
-            ->add('cci', EmailType::class, [
-                //'attr' => ['multiple' => 'multiple']
-            ])
+            ->add('cci', TextType::class, [
+                'required' => false
+            ])*/
             ->add('subject', TextType::class, [              
             ])
             ->add('content', TextareaType::class, [              
                 ])
             ->add('submit', SubmitType::class)
         ;
+
+        $builder->get('to')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($emailsAsArray): string {
+                    // transform the array to a string
+                    return implode(', ', (array)$emailsAsArray);
+                },
+                function ($emailsAsString): array {
+                    // transform the string back to an array
+                    return explode(', ', (string)$emailsAsString);
+                }
+            ));
+
+            /*$builder->get('cc')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($emailsAsArray): string {
+                    // transform the array to a string
+                    return implode(', ', (array)$emailsAsArray);
+                },
+                function ($emailsAsString): array {
+                    // transform the string back to an array
+                    return explode(', ', (string)$emailsAsString);
+                }
+            ));
+
+            $builder->get('cci')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($emailsAsArray): string {
+                    // transform the array to a string
+                    return implode(', ', (array)$emailsAsArray);
+                },
+                function ($emailsAsString): array {
+                    // transform the string back to an array
+                    return explode(', ', (string)$emailsAsString);
+                }
+            ));*/
     }
 
     public function configureOptions(OptionsResolver $resolver)
