be1040fa3e359d46641bbeb8fbd8d91b64c62a8a
Noemie-P <piccoli.noemie@gmail.com>
2023-10-23 08:33:53 +0000

    ip address + message bundle

--- migrations/Version20230922121127.php
+++ migrations/Version20230922121127.php
@@ -22,8 +22,6 @@ final class Version20230922121127 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE exercise');
-        $this->addSql('DROP TABLE ip');
         $this->addSql('ALTER TABLE lab ADD has_timer TINYINT(1) NOT NULL, ADD timer TIME DEFAULT NULL');
         $this->addSql('ALTER TABLE network_interface DROP connection, CHANGE vlan vlan INT DEFAULT 0 NOT NULL');
     }
@@ -33,10 +31,6 @@ final class Version20230922121127 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE exercise (id INT AUTO_INCREMENT NOT NULL, lab_id INT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, document VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, INDEX IDX_AEDAD51C628913D5 (lab_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('CREATE TABLE ip (id INT AUTO_INCREMENT NOT NULL, network_id INT DEFAULT NULL, INDEX IDX_A5E3B32D34128B91 (network_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE exercise ADD CONSTRAINT FK_AEDAD51C628913D5 FOREIGN KEY (lab_id) REFERENCES lab (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE ip ADD CONSTRAINT FK_A5E3B32D34128B91 FOREIGN KEY (network_id) REFERENCES network (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->addSql('ALTER TABLE lab DROP has_timer, DROP timer');
         $this->addSql('ALTER TABLE network_interface ADD connection INT DEFAULT 0 NOT NULL, CHANGE vlan vlan INT DEFAULT NULL');
     }
