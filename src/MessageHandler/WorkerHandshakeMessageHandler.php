9210a73597ba3b270e091165ed6777790dcc9183
Noemie-P <piccoli.noemie@gmail.com>
2024-07-04 10:43:15 +0200

    start physical device when restart worker

--- src/MessageHandler/WorkerHandshakeMessageHandler.php
+++ src/MessageHandler/WorkerHandshakeMessageHandler.php
@@ -7,6 +7,7 @@ use App\Instance\InstanceState;
 use Remotelabz\Message\Message\InstanceActionMessage;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\DeviceInstanceRepository;
+use App\Repository\DeviceRepository;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Remotelabz\Message\Message\WorkerHandshakeMessage;
@@ -17,6 +18,7 @@ use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
 class WorkerHandshakeMessageHandler implements MessageHandlerInterface
 {
     private $deviceInstanceRepository;
+    private $deviceRepository;
     private $entityManager;
     private $serializer;
     private $logger;
@@ -24,12 +26,14 @@ class WorkerHandshakeMessageHandler implements MessageHandlerInterface
 
     public function __construct(
         DeviceInstanceRepository $deviceInstanceRepository,
+        DeviceRepository $deviceRepository,
         EntityManagerInterface $entityManager,
         SerializerInterface $serializer,
         MessageBusInterface $bus,
         LoggerInterface $logger
     ) {
         $this->deviceInstanceRepository = $deviceInstanceRepository;
+        $this->deviceRepository = $deviceRepository;
         $this->entityManager = $entityManager;
         $this->serializer = $serializer;
         $this->logger = $logger;
@@ -50,7 +54,33 @@ class WorkerHandshakeMessageHandler implements MessageHandlerInterface
             $this->entityManager->flush();
             $uuid = $deviceInstance->getUuid();
             $context = SerializationContext::create()->setGroups('worker');
+            $device = $deviceInstance->getDevice();
             $deviceJson = $this->serializer->serialize($deviceInstance->getLabInstance(), 'json', $context);
+
+            if ($device->getVirtuality() == 0) {
+                $this->logger->info($device->getTemplate());
+                preg_match_all('!\d+!', $device->getTemplate(), $templateNumber);
+                $this->logger->info(json_encode($templateNumber, true));
+                $template = $this->deviceRepository->find($templateNumber[0][0]);
+                
+                $tmp = json_decode($deviceJson, true, 4096, JSON_OBJECT_AS_ARRAY);
+                foreach ($tmp['deviceInstances'] as $key => $tmpDeviceInstance) {
+                    if ($tmpDeviceInstance['uuid'] == $deviceInstance->getUuid()) {
+                        if ($template->getOutlet()) {
+                            $tmp['deviceInstances'][$key]['device']['outlet'] = [
+                                'outlet' => $template->getOutlet()->getOutlet(),
+                                'pdu' => [
+                                    'ip' => $template->getOutlet()->getPdu()->getIp(),
+                                    'model' => $template->getOutlet()->getPdu()->getModel(),
+                                    'brand' => $template->getOutlet()->getPdu()->getBrand()
+                                ]
+                            ];
+                        }
+                    }
+                }
+                $deviceJson = json_encode($tmp, 0, 4096);
+            }
+
             $this->logger->info('Sending device instance '.$uuid.' start message.');
             $this->logger->debug('Sending device instance '.$uuid.' start message.', json_decode($deviceJson, true));
             $this->bus->dispatch(
