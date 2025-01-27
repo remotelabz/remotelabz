7a476883754873b8ef2aba003060be501ecc142b
Florent <florent.nolot@univ-reims.fr>
2025-01-27 11:51:54 +0100

    Delete disable worker if error usage

--- src/Service/Worker/WorkerManager.php
+++ src/Service/Worker/WorkerManager.php
@@ -49,11 +49,13 @@ class WorkerManager
                 array_push($usage, $content);
             } catch (Exception $exception) {
                 $this->logger->error("Usage resources error - Web service or Worker ".$worker->getIPv4()." is not available");               
+                /*
                 $worker->setAvailable(0);
                 $entityManager = $this->doctrine->getManager();
                 $entityManager->persist($worker);
                 $entityManager->flush();
                 $this->logger->info("Worker ".$worker->getIPv4()." is disable");
+                */
             }
         }
         $this->logger->info('Usage of each worker:',$usage);
