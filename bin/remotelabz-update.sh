95fa4753521425ad228a7e317e27a6bd0cec7f90
Florent <florent.nolot@univ-reims.fr>
2025-03-15 18:46:54 +0100

    update update script

--- bin/remotelabz-update.sh
+++ bin/remotelabz-update.sh
@@ -8,6 +8,11 @@ yarn encore prod
 php bin/console doctrine:migrations:migrate
 #npx browserslist@latest --update-db
 php bin/console cache:clear
+cp config/logrotate/remotelabz /etc/logrotate.d/
+cp config/system/sudoers /etc/sudoers.d/remotelabz
+cp config/system/remotelabz.service /etc/systemd/system/
+cp config/system/remotelabz-proxy.service /etc/systemd/system/
+rm config/templates/*
 chown remotelabz:www-data * -R
 chmod g+w /opt/remotelabz/var -R
 chmod g+w /opt/remotelabz/public/uploads -R
@@ -15,6 +20,7 @@ chmod g+r config/jwt/private.pem
 sed -i '/push "route/d' /etc/openvpn/server/server.conf
 NETWORK=`awk -F "=" '/BASE_NETWORK=/{print $2}' .env.local`
 NETWORK_MASK=`awk -F "=" '/BASE_NETWORK_NETMASK=/{print $2}' .env.local`
-#echo "push \"route $NETWORK $NETWORK_MASK\"" | tee -a /etc/openvpn/server/server.conf
+echo "push \"route $NETWORK $NETWORK_MASK\"" | tee -a /etc/openvpn/server/server.conf
 systemctl daemon-reload
+service remotelabz-proxy restart
 service remotelabz restart
