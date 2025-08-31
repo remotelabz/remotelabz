7aabefe00c5be7b506570cb4af5851ef763cb840
Florent NOLOT <florent.nolot@univ-reims.fr>
2025-08-31 20:27:14 +0000

    Change in migration user_groupe json

--- migrations/Version20250831194129.php
+++ migrations/Version20250831194129.php
@@ -23,7 +23,7 @@ final class Version20250831194129 extends AbstractMigration
         $this->addSql('ALTER TABLE device ADD cdrom_iso_filename VARCHAR(255) DEFAULT NULL, ADD cdrom_bus_type VARCHAR(255) DEFAULT NULL, ADD bios_filename VARCHAR(255) DEFAULT NULL, ADD bios_type VARCHAR(255) DEFAULT NULL, ADD other_options VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE lab ADD shared TINYINT(1) NOT NULL, CHANGE version version VARCHAR(10) DEFAULT 1 NOT NULL');
         $this->addSql('ALTER TABLE operating_system ADD description LONGTEXT DEFAULT NULL, ADD arch VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE user_group CHANGE permissions permissions JSON NOT NULL');
+        //$this->addSql('ALTER TABLE user_group CHANGE permissions permissions JSON NOT NULL');
     }
 
     public function down(Schema $schema): void
@@ -32,6 +32,6 @@ final class Version20250831194129 extends AbstractMigration
         $this->addSql('ALTER TABLE device DROP cdrom_iso_filename, DROP cdrom_bus_type, DROP bios_filename, DROP bios_type, DROP other_options');
         $this->addSql('ALTER TABLE lab DROP shared, CHANGE version version VARCHAR(10) DEFAULT \'1\' NOT NULL');
         $this->addSql('ALTER TABLE operating_system DROP description, DROP arch');
-        $this->addSql('ALTER TABLE user_group CHANGE permissions permissions LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\'');
+        //$this->addSql('ALTER TABLE user_group CHANGE permissions permissions LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\'');
     }
 }
