ac18c5b04ae5490ce42310676e38d5c0e0abe9df
Florent NOLOT <florent.nolot@univ-reims.fr>
2025-09-06 16:02:52 +0000

    Noty added for editor only

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,8 @@ import 'datatables.net-bs4/css/dataTables.bootstrap4.css';
 import 'datatables.net-buttons-bs4/css/buttons.bootstrap4.css';
 import 'datatables.net-select-bs4/css/select.bootstrap4.css';
 import 'flag-icons/sass/flag-icons.scss';
+//import 'noty/src/noty.scss';
+import 'noty/src/themes/mint.scss';
 import 'simplemde/dist/simplemde.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'cropperjs/dist/cropper.min.css';
