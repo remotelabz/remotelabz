70c7f4d5765124b15b2dbc78c3f5a71269f1981c
Florent NOLOT <florent.nolot@univ-reims.fr>
2025-10-11 07:38:17 +0000

    Optimize ressource request WIP #1207

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -292,27 +292,9 @@ class ServiceController extends Controller
     public function ResourceAction(Request $request,ManagerRegistry $doctrine)
     {
         $workers = $this->configWorkerRepository->findBy(['available' => true]);
-        //$workers = explode(',', $this->workerServer);
-        //$nbWorkers = count($workers);
-      //  if ( $nbWorkers > 1) {
+        
             $usage = $this->workerManager->checkWorkersAction($doctrine);
-        //}
-        /* else {
-            $client = new Client();
-            $this->logger->debug("worker:".$workers[0]->getIPv4());
-
-            $url = 'http://'.$workers[0]->getIPv4().':'.$this->workerPort.'/stats/hardware';
-            $response="";
-            try {
-                $response = $client->get($url);
-                $usage = json_decode($response->getBody()->getContents(), true);
-            } catch (Exception $exception) {
-                $this->addFlash('danger', 'Worker is not available');
-                $this->logger->error("Usage resources error - Web service or Worker ".$worker[0]->getIPv4()." is not available");
-                $usage=null;
-            }
-
-        }*/
+       
         $this->logger->debug("worker usage:",$usage);
 
         return $this->render('service/resources.html.twig', [
